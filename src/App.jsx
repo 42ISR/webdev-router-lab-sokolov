b491798d09861b9909ff1e091f8aa7ed9b557bf1
f1zery <felik98@inbox.ru>
2026-09-11 05:15:24 +0000

    132

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import Header from './components/Header.jsx';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout.jsx';
 import HomePage from './pages/HomePage.jsx';
+import CatalogPage from './pages/CatalogPage.jsx';
+import ProductPage from './pages/ProductPage.jsx';
+import SearchPage from './pages/SearchPage.jsx';
+import AboutPage from './pages/AboutPage.jsx';
+import ContactsPage from './pages/ContactsPage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <main className="content">
-        <HomePage />
-      </main>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="catalog" element={<CatalogPage />} />
+        <Route path="catalog/:id" element={<ProductPage />} />
+        <Route path="search" element={<SearchPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="contacts" element={<ContactsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
 }
